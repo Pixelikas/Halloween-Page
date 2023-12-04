46baa89176ac8dd717b062f19d9c07befdc846e7
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2023-12-04 09:06:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let input = document.getElementById("input")
+let button = document.getElementById("button")
+
+function Cadastra(){
+
+    input.value = ""
+    alert("Boa! Você está cadastrado :D")
+
+}
+
+function Facebook(){
+
+    window.open("http://facebook.com")
+
+}
+
+function Twitter(){
+
+    window.open("http://twitter.com")
+
+}
+
+function Instagram(){
+
+    window.open("http://instagram.com")
+
+}
